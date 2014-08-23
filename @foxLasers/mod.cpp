0054b3394d69ed945d407a57daad371de1445b1d
Alex Strout <alex@taraxis.com>
2014-08-22 21:59:17 -0400

    add other files

---
+++ @foxLasers/mod.cpp
@@ -0,0 +1,17 @@
+name = "Universal Virtual Armory / AI Tweaks";
+version = "0.01";
+author = "fox";
+actionName = "Herp Derp";
+//action = "http://www.88flak.com/";
+description = "This is a test description!";
+overview = "This is a test overview!";
+overviewText = "This is a test overviewText!";
+overviewFootnote = "This is a test overviewFootnote!";
+tooltip = "This is a test tooltip!";
+tooltipOwned = "This is a test tooltipOwned!";
+//picture = "mod.paa";
+//overviewPicture = "mod_overview.paa";
+//logo = "mod_logo.paa";
+//logoOver = "mod_over.paa";
+//dir = "";
+//infoPages[] = {};
---
+++ @foxLasers/userconfig/settings.hpp
@@ -0,0 +1,5 @@
+//fox AI Tweaks Config
+aiPlayerGroupForceLasers = 1; //Force-enable IR Lasers for all AI in player's group (SP/MP)
+aiPlayerGroupForceSkill = 1; //Force high skill for all AI in player's group (SP/MP)
+aiPlayableUnitsForceLasers = 0; //Force-enable IR Lasers for all AI in playable units (MP Only)
+aiPlayableUnitsForceSkill = 1; //Force high skill for all AI in playable units (MP Only)
